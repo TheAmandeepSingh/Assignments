d3ae72e0500955f22b6c80a40fca8171d5c2dd58
TheAmandeepSingh <amandeep.gini@yahoo.com>
2022-02-06 22:42:44 +0530

    playing around

--- Personal/Inheritance.cpp
+++ Personal/Inheritance.cpp
@@ -1,2 +1,49 @@
 #include <iostream>
+using namespace std;
 
+class Base
+{
+    int a{};
+    
+public:
+    Base() { cout << "Base constructor" << endl; }
+   
+
+    void fun() { cout << "Base Fun" << endl; }
+
+protected:
+    ~Base() { cout << "Base Destructor" << endl; }
+};
+
+class Derived : public Base
+{
+    int b{};
+
+public:
+    Derived() { cout << "Derived constructor" << endl; }
+    ~Derived() {cout<< "Derived destructor" << endl;}
+    void fun() { cout << "Derived fun" << endl; }
+};
+
+struct A
+{
+protected:
+    ~A() = default; // based class has protected destructor
+};
+
+struct B : A
+{
+};
+
+B b{}; // Well-formed?
+
+int main()
+{
+    // Base base;
+    // base.fun();
+
+    Derived derived;
+    derived.fun();
+
+    return 0;
+}
